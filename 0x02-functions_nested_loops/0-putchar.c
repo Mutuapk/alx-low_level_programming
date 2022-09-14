5d7b66d5c7348bd1ad470336d435d83c28448988
Peter Mutua <mutuapeterk@gmail.com>
2022-09-14 12:16:54 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print main
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+_putchar('M');
+_putchar('a');
+_putchar('i');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
