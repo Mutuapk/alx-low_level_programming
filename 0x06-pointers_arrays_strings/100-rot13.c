c3f01958e08613e7fe1e0ed9f9f14d19f303b3ff
Peter Mutua <mutuapeterk@gmail.com>
2022-09-21 22:49:38 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- *rot13 - encodes a string using rot13.
+ * *rot13 - encodes a string using rot13.
+ *
  * @str: string.
  * Return: string.
  */
-
 char *rot13(char *str)
 {
 int i;
